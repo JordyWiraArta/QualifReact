6dcecd1b89b6dc7b2f5957f5ba3d62fa1d5027a3
JordyWiraArta <98179507+JordyWiraArta@users.noreply.github.com>
2022-07-25 14:00:23 +0700

    qualif

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -6,12 +6,17 @@ export default function Header(){
 
     const path = window.location.pathname;
     return <div>
-        {path === "/" && <Link 
+        {path === "/"? <Link 
         to="/favorite"
         id="btnFavorite"
         >
             Favorites
-        </Link> }
+        </Link> : <Link 
+        to="/"
+        id="btnBack"
+        >
+            Back to Home
+        </Link>}
 
         <div id="title">
             <img src="https://pngimg.com/uploads/pokemon_logo/pokemon_logo_PNG12.png" alt="" height={40}/>
